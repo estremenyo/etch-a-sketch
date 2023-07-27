981574cd057ec2dd5120473b372ce5141cb126c5
Estremenyo <109721927+estremenyo@users.noreply.github.com>
2023-07-27 12:12:16 +0400

    Allow user to generate new grid

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const container = document.querySelector(".container");
+generateGrid(16);
+const button = document.querySelector(".promptButton");
+button.addEventListener("click", handleButton);
 
-function generateGrid(squares) {
+function generateGrid(squaresPerLine) {
     Array.from(container.children).forEach(child => child.remove());
-    for (let i = 0; i < squares * squares; i++) {
+
+    for (let i = 0; i < squaresPerLine * squaresPerLine; i++) {
         let div = document.createElement("div");
         div.setAttribute("class", "gridElement");
+
+        let dimensions = Math.floor((100 / squaresPerLine) * 100) / 100;
+        div.style.height = `${dimensions}%`;
+        div.style.width = `${dimensions}%`;
+
         container.appendChild(div);
     }
+
+
+    // CSS Manipulation goes here
+    // generateGrid() only works with 16 as arg because the
+    // CSS width & height values for each gridElement are hardcoded
+    // The task is to change those values based on user input and dimensions of container
+
+
+
     const gridList = document.querySelectorAll("div.gridElement");
     gridList.forEach(element => element.addEventListener("mouseover", handleMouseover));  
 }
 
-generateGrid(16);
-
-
 function handleMouseover(e) {
-    e.target.setAttribute("class", "hovered");
+    e.target.classList.add("hovered");
 }
 
-const button = document.querySelector(".promptButton");
-button.addEventListener("click", handleButton);
 
 function handleButton() {
     let response = "";
-    while (isNaN(response) || !Number.isInteger(response) || response > 100 || response < 0) {
+    while (isNaN(response) || !Number.isInteger(response) ||
+    response > 100 || response < 0 || response == "") {
         response = Number(prompt("How many squares per line? (Default 16, Max 100)"));
     }
     generateGrid(response);
 }
+
