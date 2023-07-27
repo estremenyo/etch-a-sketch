b56d1e4086f764010d0fca48d2e9b598795910ac
Estremenyo <109721927+estremenyo@users.noreply.github.com>
2023-07-27 13:05:24 +0400

    Add change to random color on mouseover

--- script.js
+++ script.js
@@ -8,29 +8,22 @@ function generateGrid(squaresPerLine) {
 
     for (let i = 0; i < squaresPerLine * squaresPerLine; i++) {
         let div = document.createElement("div");
-        div.setAttribute("class", "gridElement");
 
         let dimensions = Math.floor((100 / squaresPerLine) * 100) / 100;
         div.style.height = `${dimensions}%`;
         div.style.width = `${dimensions}%`;
 
+        div.addEventListener("mouseover", handleMouseover);
         container.appendChild(div);
     }
-
-
-    // CSS Manipulation goes here
-    // generateGrid() only works with 16 as arg because the
-    // CSS width & height values for each gridElement are hardcoded
-    // The task is to change those values based on user input and dimensions of container
-
-
-
-    const gridList = document.querySelectorAll("div.gridElement");
-    gridList.forEach(element => element.addEventListener("mouseover", handleMouseover));  
 }
 
 function handleMouseover(e) {
-    e.target.classList.add("hovered");
+    let colorR = Math.floor(Math.random() * 256);
+    let colorG = Math.floor(Math.random() * 256);
+    let colorB = Math.floor(Math.random() * 256);
+
+    e.target.style.background = `rgb(${colorR}, ${colorG}, ${colorB})`;
 }
 
 
